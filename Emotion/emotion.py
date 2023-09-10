ad18c38b832f6c9c41674cab928d50844cc084b7
Halil İbrahim <79845872+Halil3509@users.noreply.github.com>
2023-09-10 15:31:04 +0300

    emotion logger update

--- Emotion/emotion.py
+++ Emotion/emotion.py
@@ -2,13 +2,21 @@ import cv2
 import numpy as np
 import base64
 from FinalModel import PsynexaModel
+import logging
 
 class EmotionDetection():
     def __init__(self):
         self.resutl_dict = dict()
         self.psynexa_model = PsynexaModel()
         
-
+    @property
+    def logger(self):
+        if not hasattr(self, '_logger'):
+            self._logger = logging.getLogger(__name__)
+            # Configure logging settings for this logger
+            logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+        return self._logger
+    
     
     def api_recognition(self, image_url):
         
@@ -19,5 +27,7 @@ class EmotionDetection():
         img = cv2.imdecode(image_content, cv2.IMREAD_COLOR)
 
         results = self.psynexa_model.detect_emotion_for_single_frame(img)
+        
+        self.logger.info("Emoteion Detection Processes has been completed.")
         
         return results
