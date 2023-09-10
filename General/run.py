bc88b7be0c3797f3c41b66cd2b6c1f500852db24
Halil İbrahim <79845872+Halil3509@users.noreply.github.com>
2023-09-10 16:29:57 +0300

    Add full_disorder_detection method

--- General/run.py
+++ General/run.py
@@ -1,21 +1,27 @@
 import numpy as np
 import logging
 import yaml
+import torch
+import whisper
 
 import EyeDetection
 import HeadOscillation
 import SpeechSpeed
 import Parkinson
 import DemantiaClockTest
+import TopicSegmentation
+import DisorderDetection
 
 class Run():
     def __init__(self, video_path, audio_path, fps = 2):
+        self._audio_path = audio_path
         self._eye_class = EyeDetection.ADHD_Calculation(video_path=video_path,fps = fps)
         self._head_class = HeadOscillation.Head(video_path=video_path, fps = fps)
         self._speech_speed_class = SpeechSpeed.SpeechSpeedClass(audio_path=audio_path)
         self._spiral_parkinson_class = Parkinson.ParkinsonDetection(type = "spiral")
         self._wave_parkinson_class = Parkinson.ParkinsonDetection(type = "wave")
         self._dementia_clock_test_class = DemantiaClockTest.DemantiaClockTestClass() # model_path parameter have already arranged
+        self._whisper_model = self.get_whisper_model()
         self._ratios = self.get_ratios()
         
     @property
@@ -25,6 +31,27 @@ class Run():
             # Configure logging settings for this logger
             logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
         return self._logger
+    
+    
+    def get_whisper_model(self):
+
+        torch.cuda.is_available()
+        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.logger.info(f"The device is {DEVICE}. ")
+
+        self.logger.info("Model is loading...")
+        model = whisper.load_model("medium", device = DEVICE)
+        self.logger.info("Model is ready :)")
+
+        return model
+    
+    def speech_to_text(self):
+        
+        # Speech to text process
+        self._converted_pure_text = self._whisper_model.transcribe(self._audio_path)['text']
+        self.logger.info("Speech to text process has been finisehd succesfully. ")
+        
     
     
     def get_ratios(self, path = r"D:\\Psynexa-AI-Github\\General\\ratios.yaml"):
@@ -54,7 +81,26 @@ class Run():
         
         
         # Disorder Calculation
+    
+    
+    def full_disorder_detection(self):
         
+        # Speech to Text Part
+        self.speech_to_text()
+        
+        # Convert text and eliminate  (Topic Side)
+        topic_class = TopicSegmentation.TopicSegmentation(self._converted_pure_text)
+        topic_texts = topic_class.convert_text_and_eliminate()
+        
+        # Disorder Part
+        disorder_class = DisorderDetection.DisorderDetection(topic_texts=topic_texts)
+        total_disorder_result_json = disorder_class.run_disorder_detection()
+        
+        self.logger.info("Full Disorder Detection process was finished.")
+        
+        return total_disorder_result_json
+   
+
     
     def statistical_touch(self):
         
